2c6e18b1ac259154d3f285caf9f9277f3d77ec13
gary <baby_plant@icloud.com>
2021-04-10 11:48:46 -0700

    Complete 3

--- starter/problems/problem-03-make-evil-dragons.js
+++ starter/problems/problem-03-make-evil-dragons.js
@@ -2,6 +2,51 @@
 
 Let's create an class to represent a EvilDragon! 🐉
 
+
+In addition to Mocha, we recommend that you test your code manually using
+Node.js with the examples below. Use the command:
+
+`node problems/03-make-evil-dragons.js`
+
+Examples:
+
+const enderDragon = new EvilDragon("Ender Dragon", "black", ["spit a fireball at
+Steve", "rule over all the Endermen", "destroy all blocks in its way"],
+"Steve"); console.log(enderDragon);
+enderDragon.dontInviteThemOverForDinner();
+console.log(enderDragon.breathesFire());
+console.log(enderDragon.burnsNemesis());
+
+// Should print...
+
+//  EvilDragon {//    name: 'Ender Dragon', //    color: 'black', //
+causesTrouble: [//      'spit a fireball at Steve', //      'rule over all the
+Endermen', //      'destroy all blocks in its way'
+//    ],
+//    nemesis: 'Steve'
+//  }
+//  Ender Dragon will spit a fireball at Steve //  Ender Dragon will rule over
+all the Endermen //  Ender Dragon will destroy all blocks in its way //  Ender
+Dragon breathes fire everywhere! BURN!!!! //  Ender Dragon destroys Steve with
+fire! WHOOOSH!!!
+
+const drogon = new EvilDragon("Drogon", "black-red",["eat all your livestock",
+"burn down your castle in Westeros","burn your father and brother"],"Night
+King"); console.log(drogon); drogon.dontInviteThemOverForDinner();
+console.log(drogon.breathesFire()); console.log(drogon.burnsNemesis());
+
+// Should print...
+
+//  EvilDragon {//    name: 'Drogon', //    color: 'black-red', //
+causesTrouble: [//      'eat all your livestock', //      'burn down your castle
+in Westeros', //      'burn your father and brother'
+//    ],
+//    nemesis: 'Night King'
+//  }
+//  Drogon will eat all your livestock //  Drogon will burn down your castle in
+Westeros //  Drogon will burn your father and brother //  Drogon breathes fire
+everywhere! BURN!!!! //  Drogon destroys Night King with fire! WHOOOSH!!!
+
 1. Require the Dragon class from the `./problem-01-make-dragons.js` module.
 
 2. Define an class named `EvilDragon` that inherits from the `Dragon` parent
@@ -26,55 +71,25 @@ WILL THROW A 'TypeError` AND NOT RUN***
     `nemesis` property values formatted as "${name} destroys ${nemesis} with
     fire! WHOOOSH!!!".
 
-In addition to Mocha, we recommend that you test your code manually using
-Node.js with the examples below. Use the command:
-
-`node problems/03-make-evil-dragons.js`
-
-Examples:
-
-const enderDragon = new EvilDragon("Ender Dragon", "black", ["spit a fireball at
-    Steve", "rule over all the Endermen", "destroy all blocks in its way"],
-    "Steve"); console.log(enderDragon);
-    enderDragon.dontInviteThemOverForDinner();
-    console.log(enderDragon.breathesFire());
-    console.log(enderDragon.burnsNemesis());
-
-// Should print...
-
-//  EvilDragon {//    name: 'Ender Dragon', //    color: 'black', //
-causesTrouble: [//      'spit a fireball at Steve', //      'rule over all the
-Endermen', //      'destroy all blocks in its way'
-//    ],
-//    nemesis: 'Steve'
-//  }
-//  Ender Dragon will spit a fireball at Steve //  Ender Dragon will rule over
-all the Endermen //  Ender Dragon will destroy all blocks in its way //  Ender
-Dragon breathes fire everywhere! BURN!!!! //  Ender Dragon destroys Steve with
-fire! WHOOOSH!!!
-
-const drogon = new EvilDragon("Drogon", "black-red",["eat all your livestock",
-    "burn down your castle in Westeros","burn your father and brother"],"Night
-    King"); console.log(drogon); drogon.dontInviteThemOverForDinner();
-    console.log(drogon.breathesFire()); console.log(drogon.burnsNemesis());
-
-// Should print...
-
-//  EvilDragon {//    name: 'Drogon', //    color: 'black-red', //
-causesTrouble: [//      'eat all your livestock', //      'burn down your castle
-in Westeros', //      'burn your father and brother'
-//    ],
-//    nemesis: 'Night King'
-//  }
-//  Drogon will eat all your livestock //  Drogon will burn down your castle in
-Westeros //  Drogon will burn your father and brother //  Drogon breathes fire
-everywhere! BURN!!!! //  Drogon destroys Night King with fire! WHOOOSH!!!
-
-
 *************** YOUR CODE BELOW ***************************************/
+const Dragon = require('./problem-01-make-dragons.js')
 
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color)
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
 
+    }
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(doing => {
+            console.log(`${this.name} will ${doing}`)
+        });
+    }
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+}
 
-
-
+module.exports = EvilDragon;
 /*********************************************************************/
